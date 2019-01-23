428fae078bd730b4a0770eae744d8c48480dfa3f
FengJing <448195466@qq.com>
2019-01-23 15:53:34 +0800

    .

---
+++ src/components/form/form-item.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+import AsyncValidator from 'async-validator';
+import Emitter from '../../mixins/emitter.js';
+export default {
+  name: 'iFormItem',
+  mixins: [Emitter],
+  inject: ['form'],
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    prop: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      validateState: '',  // 校验状态
+      validateMessage: '',  // 校验不通过时的提示信息
+    }
+  },
+  computed: {
+    // 从 Form 的 model 中动态得到当前表单组件的数据
+    fieldValue () {
+      return this.form.model[this.prop];
+    }
+  },
+  methods: {
+    // 从 Form 的 rules 属性中，获取当前 FormItem 的校验规则
+    getRules () {
+      let formRules = this.form.rules;
+
+      formRules = formRules ? formRules[this.prop] : [];
+
+      return [].concat(formRules || []);
+    },
+    // 只支持 blur 和 change，所以过滤出符合要求的 rule 规则
+    getFilteredRule (trigger) {
+      const rules = this.getRules();
+      return rules.filter(rule => !rule.trigger || rule.trigger.indexOf(trigger) !== -1);
+    },
+    /**
+     * 校验数据
+     * @param trigger 校验类型
+     * @param callback 回调函数
+     */
+    validate (trigger, callback = function () { }) {
+      let rules = this.getFilteredRule(trigger);
+
+      if (!rules || rules.length === 0) {
+        return true;
+      }
+
+      // 设置状态为校验中
+      this.validateState = 'validating';
+
+      // 以下为 async-validator 库的调用方法
+      let descriptor = {};
+      descriptor[this.prop] = rules;
+
+      const validator = new AsyncValidator(descriptor);
+      let model = {};
+
+      model[this.prop] = this.fieldValue;
+
+      validator.validate(model, { firstFields: true }, errors => {
+        this.validateState = !errors ? 'success' : 'error';
+        this.validateMessage = errors ? errors[0].message : '';
+
+        callback(this.validateMessage);
+      });
+    },
+    onFieldBlur () {
+      this.validate('blur');
+    },
+    onFieldChange () {
+      this.validate('change');
+    },
+    setRules () {
+      this.$on('on-form-blur', this.onFieldBlur);
+      this.$on('on-form-change', this.onFieldChange);
+    },
+  },
+  mounted () {
+    // 如果没有传入 prop，则无需校验，也就无需缓存
+    if (this.prop) {
+      this.dispatch('iForm', 'on-form-item-add', this);
+      this.setRules();
+    }
+  },
+  // 组件销毁前，将实例从 Form 的缓存中移除
+  beforeDestroy () {
+    this.dispatch('iForm', 'on-form-item-remove', this);
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form :model="formValidate"
+          :rules="ruleValidate">
+      <FormItem label="用户名"
+                prop="name">
+        <Input v-model="formValidate.name" />
+      </FormItem>
+      <FormItem label="邮箱"
+                prop="mail">
+        <Input v-model="formValidate.mail" />
+      </FormItem>
+    </Form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      formValidate: {
+        name: '',
+        mail: ''
+      },
+      ruleValidate: {
+        name: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' }
+        ],
+        mail: [
+          { required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+        ],
+      }
+    }
+  },
   components: {
-    HelloWorld
+    Form: () => import('../components/form/form.vue'),
+    FormItem: () => import('../components/form/form-item.vue'),
+    Input: () => import('../components/input/input.vue')
   }
 }
 </script>
