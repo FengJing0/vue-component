1f92b75c39a77de95899374eb142f390f235c55b
FengJing <448195466@qq.com>
2019-10-26 16:22:23 +0800

    test

---
+++ dist/js/chunk-2d20f6aa.45d1a0c4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20f6aa"],{b413:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",[e("div",[t._t("header")],2),e("div",[t._t("default",null,{data:t.data})],2),e("div",[t._t("footer")],2)])},u=[],d={data:function(){return{data:{name:"tom"}}}},l=d,o=e("2877"),i=Object(o["a"])(l,n,u,!1,null,null,null);i.options.__file="slot.vue";a["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d20f6aa.45d1a0c4.js.map
--- src/views/tree.vue
+++ src/views/tree.vue
@@ -1,4 +1,4 @@
-<!-- src/views/alert.vue -->
+
 <template>
   <i-tree :data='data'
           :showCheckbox='true'
